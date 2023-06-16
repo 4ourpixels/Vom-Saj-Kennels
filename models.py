12bd86d3995ff83e00335e479cce30fd60fdceb4
Moses Bartena Towet <121856163+4ourpixels@users.noreply.github.com>
2023-06-16 22:14:44 +0300

    Update models.py

--- models.py
+++ models.py
@@ -5,7 +5,6 @@ class Dog(models.Model):
     name = models.CharField(max_length=100)
     breed = models.CharField(max_length=100)
     owner = models.CharField(max_length=100)
-    age = models.DateField()
     age = models.PositiveIntegerField()
     price = models.DecimalField(max_digits=8, decimal_places=2)
     description = models.TextField()
